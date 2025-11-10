5c597f7ceed6d65ab0f5411ed1d9a0cb4d669312
csae8092 <p.andorfer@gmail.com>
2025-11-10 10:15:55 +0100

    data updates

--- pyscripts/copy_files.py
+++ pyscripts/copy_files.py
@@ -4,22 +4,15 @@ import shutil
 
 data = "data"
 editions = os.path.join(data, "editions")
-indices = os.path.join(data, "indices")
-meta = os.path.join(data, "meta")
-listbibl = "../wiener-rundschau-data/data/indices/listbibl.xml"
-about = "../wiener-rundschau-data/data/meta/about.xml"
+shutil.rmtree(data, ignore_errors=True)
 
-
-shutil.rmtree(data)
-os.makedirs(editions)
-os.makedirs(indices)
-os.makedirs(meta)
+data_path = "../wiener-rundschau-data/data"
+shutil.copytree(data_path, data)
+shutil.rmtree(editions, ignore_errors=True)
+os.makedirs(editions, exist_ok=True)
 
 
 files = glob.glob("../wiener-rundschau-data/data/editions/*/*.xml")
 for x in files:
     target_path = os.path.join(editions, os.path.basename(x))
     shutil.copy2(x, target_path)
-
-shutil.copy2(listbibl, os.path.join(indices, "listbibl.xml"))
-shutil.copy2(about, os.path.join(meta, "about.xml"))
